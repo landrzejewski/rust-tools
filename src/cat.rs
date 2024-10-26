76bc5d43725e97b9709589052c0f05be23cc2548
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-26 14:36:47 +0200

    part 6

---
+++ src/cat.rs
@@ -0,0 +1,82 @@
+use std::{env, fs::File, io::{BufRead, BufReader}};
+
+const ARG_PREFIX: &str = "-";
+
+enum Mode {
+    Default,
+    WithNumbering { igonre_empty_lines: bool }
+}
+
+impl Mode {
+    
+    fn from(options: &Vec<String>) -> Mode {
+        if options.is_empty() {
+            Mode::Default
+        } else {
+            match options[0].as_str() {
+                "-n" => Mode::WithNumbering { igonre_empty_lines: false },
+                "-nb" => Mode::WithNumbering { igonre_empty_lines: true },
+                _ => Mode::Default
+            }
+        }
+    }
+
+}
+
+fn main() {
+    let (options, paths) = get_input();
+    if paths.is_empty() {
+        show_help();
+        return;
+    }
+    let mode = Mode::from(&options);
+    cat(&mode, &paths);
+}
+
+fn get_input() -> (Vec<String>, Vec<String>) {
+    env::args().skip(1).partition(|arg| arg.starts_with(ARG_PREFIX))
+}
+
+fn show_help() {
+    println!("Usage:");
+    println!("cat [option] file1, file2 ...");
+    println!("options:");
+    println!("  -n - show line numbers");
+    println!("  -nb - show line numbers, ignore blank lines");
+}
+
+fn cat(mode: &Mode, paths: &Vec<String>) {
+    let print_line = match mode {
+        Mode::WithNumbering { igonre_empty_lines: false } => print_with_line_numbers,
+        Mode::WithNumbering { igonre_empty_lines: true } => print_with_line_numbers_ignoring_empty_lines,
+        _ => print_line
+    };
+    for path in paths {
+        let Ok(file) = File::open(path) else {
+            println!("Failed to open {path}");
+            continue;
+        };
+        println!("File: {path}");
+        let reader = BufReader::new(file);  
+        for (line_number, line) in reader.lines().enumerate() {
+            let line_text = line.expect("Failed to read line");
+            print_line(line_number + 1, &line_text);
+        }
+    }
+}
+
+fn print_line(_line_number: usize, line: &String) {
+    println!("{line}");
+}
+
+fn print_with_line_numbers(line_number: usize, line: &String) {
+    println!("{:3}:\t{}", line_number, line);
+}
+
+fn print_with_line_numbers_ignoring_empty_lines(line_number: usize, line: &String) {
+    if line.is_empty() {
+        println!();
+    } else {
+        print_with_line_numbers(line_number, line);
+    }
+}
